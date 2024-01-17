e197883472b08445097984182cf9938225724650
zjl <870667674@qq.com>
2024-01-17 16:46:29 +0800

    基本配置

---
+++ rollup.config.js
@@ -0,0 +1,14 @@
+import typescript from "@rollup/plugin-typescript"
+
+export default {
+  input: "src/index.ts",
+  output: {
+    file: "dist/index.js",
+    format: "module",
+    sourcemap: true,
+  },
+  external: [/node_modules/],
+  plugins: [
+    typescript(),
+  ]
+}
