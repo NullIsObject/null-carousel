dad76e27d5f896e4aab0f1773ab0abbe693a1f9b
SNIVY <870667674@qq.com>
2024-03-10 16:57:34 +0800

    打包输出readme和package.json

--- build/gulpfile.ts
+++ build/gulpfile.ts
@@ -4,16 +4,24 @@ import {
 import {
   build as vite
 }                from "vite"
-import viteConfig, {
-  root, outDir
-}                from "./vite.config"
 import {exec}    from "child_process"
 import {resolve} from "path"
-import {remove}  from "fs-extra"
+import {
+  remove, writeJson, mkdirs, copy
+}                from "fs-extra"
+import vue       from "@vitejs/plugin-vue"
+
+const root = resolve(__dirname, "../")
+const outDir = "dist/packages"
+
+export async function initOutDir() {
+  await remove(resolve(root, outDir))
+  await mkdirs(resolve(root, outDir))
+}
 
 export function tsc() {
   return new Promise((resolve, reject) => {
-    exec("vue-tsc", (error, stdout, stderr) => {
+    exec("vue-tsc -p ./tsconfig.packages.json", (error, stdout, stderr) => {
       if (error) reject(error)
       else if (stderr) reject(stderr)
       else resolve(stdout)
@@ -22,11 +30,42 @@ export function tsc() {
 }
 
 export function build() {
-  return vite(viteConfig)
+  return vite({
+    root,
+    plugins: [
+      vue(),
+    ],
+    build: {
+      outDir,
+      emptyOutDir: false,
+      lib: {
+        entry: ["packages"],
+        name: "null-carousel",
+        formats: ["es"]
+      },
+      rollupOptions: {
+        external: ["vue"],
+        output: {
+          preserveModules: true,
+          format: "module",
+        },
+      },
+    }
+  })
 }
 
-export function deleteOutDir() {
-  return remove(resolve(root, outDir))
+export function outPkgJSON() {
+  const packageJSON = require("packages/package.json")
+  const rootConfig = require("root/package.json")
+  const outputPath = resolve(root, outDir, "package.json")
+  packageJSON.name = rootConfig.name
+  return writeJson(outputPath, packageJSON, {spaces: 2})
 }
 
-export default series(deleteOutDir, parallel(tsc, build))
+export function outReadme() {
+  const src = resolve(root, "README.md")
+  const dest = resolve(root, outDir, "README.md")
+  return copy(src, dest)
+}
+
+export default series(initOutDir, parallel(tsc, build, outPkgJSON, outReadme))
