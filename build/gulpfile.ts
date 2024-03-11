ebbfe3fc6e1316afc4bbe89272d5fffc36771c01
SNIVY <870667674@qq.com>
2024-03-12 00:47:34 +0800

    增加vue类型提示

--- build/gulpfile.ts
+++ build/gulpfile.ts
@@ -1,33 +1,33 @@
 import {
   series, parallel
-}                      from "gulp"
+}           from "gulp"
 import {
   build as vite
-}                      from "vite"
-import path, {resolve} from "path"
+}           from "vite"
+import path from "path"
 import {
   remove, writeJson, mkdirs, copy
-}                      from "fs-extra"
-import vue             from "@vitejs/plugin-vue"
+}           from "fs-extra"
+import vue  from "@vitejs/plugin-vue"
 import {
   createProgram,
-}                      from "vue-tsc"
+}           from "vue-tsc"
 import {
   createCompilerHost, CompilerOptions
-}                      from "typescript"
+}           from "typescript"
 
-const root = resolve(__dirname, "../")
+const root = path.resolve(__dirname, "../")
 const outDir = "dist/packages"
 const entryDir = "packages"
 
 export async function initOutDir() {
-  await remove(resolve(root, outDir))
-  await mkdirs(resolve(root, outDir))
+  await remove(path.resolve(root, outDir))
+  await mkdirs(path.resolve(root, outDir))
 }
 
 export function tsc() {
   const compilerOptions: CompilerOptions = {
-    outDir: path.join(root, outDir),
+    outDir: path.resolve(root, outDir),
     allowJs: true,
     declaration: true,
     incremental: true,
@@ -36,7 +36,7 @@ export function tsc() {
     emitDeclarationOnly: true,
   }
   return Promise.resolve(createProgram({
-    rootNames: [resolve(root, entryDir, "index")],
+    rootNames: [path.resolve(root, entryDir, "index")],
     options: compilerOptions,
     host: createCompilerHost(compilerOptions)
   }).emit())
@@ -70,7 +70,7 @@ export function build() {
 export function outPkgJSON() {
   const rootPkg = require("root/package.json")
   const packagesPkg = require("packages/package.json")
-  const outputPath = resolve(root, outDir, "package.json")
+  const outputPath = path.resolve(root, outDir, "package.json")
   const finalPkg = {
     ...rootPkg,
     ...packagesPkg,
@@ -86,8 +86,8 @@ export function outPkgJSON() {
 }
 
 export function outReadme() {
-  const src = resolve(root, "README.md")
-  const dest = resolve(root, outDir, "README.md")
+  const src = path.resolve(root, "README.md")
+  const dest = path.resolve(root, outDir, "README.md")
   return copy(src, dest)
 }
 
