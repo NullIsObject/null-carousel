a637def6aab0e3ba8ed5e32f000169db6fbaebe7
SNIVY <870667674@qq.com>
2024-03-23 16:27:20 +0800

    .d.ts路径别名替换

--- build/gulpfile.ts
+++ build/gulpfile.ts
@@ -1,29 +1,26 @@
 import {
   series, parallel
-}              from "gulp"
+}           from "gulp"
 import {
   build as vite
-}              from "vite"
-import path    from "path"
-import {
-  remove, writeJson, mkdirs, copy
-}              from "fs-extra"
-import vue     from "@vitejs/plugin-vue"
+}           from "vite"
+import path from "path"
+import fs   from "fs-extra"
+import vue  from "@vitejs/plugin-vue"
 import {
   createProgram,
-}              from "vue-tsc"
+}           from "vue-tsc"
 import {
   createCompilerHost, CompilerOptions
-}              from "typescript"
-import * as fs from 'fs'
+}           from "typescript"
 
 const root = path.resolve(__dirname, "../")
 const outDir = "dist/packages"
 const entryDir = "packages"
 
 export async function initOutDir() {
-  await remove(path.resolve(root, outDir))
-  await mkdirs(path.resolve(root, outDir))
+  await fs.remove(path.resolve(root, outDir))
+  await fs.mkdirs(path.resolve(root, outDir))
 }
 
 export async function tsc() {
@@ -33,14 +30,28 @@ export async function tsc() {
     declaration: true,
     incremental: true,
     // skipLibCheck: true,
-    strictNullChecks: true,
+    strict: true,
     emitDeclarationOnly: true,
   }
-  return createProgram({
-    rootNames: readFilesRecursive(path.resolve(root, entryDir)),
-    options: compilerOptions,
-    host: createCompilerHost(compilerOptions)
-  }).emit()
+  {
+    const include = [".vue", ".ts", ".tsx"]
+    const dir = path.resolve(root, entryDir)
+    createProgram({
+      rootNames: readFilesRecursive({dir, include}),
+      options: compilerOptions,
+      host: createCompilerHost(compilerOptions)
+    }).emit()
+  }
+  {
+    const include = [".d.ts"]
+    const dir = path.resolve(root, outDir)
+    const fileList = readFilesRecursive({dir, include})
+    for (const fileName of fileList) {
+      let content = fs.readFileSync(fileName).toString()
+      content = content.replaceAll("@null-carousel/packages", "null-carousel")
+      fs.writeFileSync(fileName, content)
+    }
+  }
 }
 
 export function build() {
@@ -69,8 +80,8 @@ export function build() {
 }
 
 export function outPkgJSON() {
-  const rootPkg = require("@null-carousel/root/package.json")
-  const packagesPkg = require("@null-carousel/packages/package.json")
+  const rootPkg: Record<string, any> = require("@null-carousel/root/package.json")
+  const packagesPkg: Record<string, any> = require("@null-carousel/packages/package.json")
   const outputPath = path.resolve(root, outDir, "package.json")
   const finalPkg = {
     ...rootPkg,
@@ -83,26 +94,25 @@ export function outPkgJSON() {
   finalPkg.packageManager = void 0
   finalPkg.scripts = void 0
   finalPkg.engines = void 0
-  return writeJson(outputPath, finalPkg, {spaces: 2})
+  return fs.writeJson(outputPath, finalPkg, {spaces: 2})
 }
 
 export function outReadme() {
   const src = path.resolve(root, "README.md")
   const dest = path.resolve(root, outDir, "README.md")
-  return copy(src, dest)
+  return fs.copy(src, dest)
 }
 
-function readFilesRecursive(dir: string) {
+function readFilesRecursive({dir, include}: { dir: string, include: string[] }) {
   const result: string[] = []
 
   for (const file of fs.readdirSync(dir)) {
-    if (file.endsWith(".json") || file === "node_modules") {
-      continue
-    }
     const filepath = path.join(dir, file)
     const stat = fs.statSync(filepath)
+    const isInclude = include.some(fileSuffix => file.endsWith(fileSuffix))
+    if (stat.isFile() && !isInclude || file === "node_modules") continue
     if (stat.isDirectory()) {
-      result.push(...readFilesRecursive(filepath))
+      result.push(...readFilesRecursive({dir: filepath, include}))
     } else {
       result.push(filepath)
     }
--- packages/index.ts
+++ packages/index.ts
@@ -1,6 +1,6 @@
 import useCarousel from "@null-carousel/packages/utils/use-carousel"
 
-export * from "./components/index"
+export * from "@null-carousel/packages/components/index"
 export {
   useCarousel
 }
