e2937027b3960160e7b3f3dfc85e82f6e3a70de2
zjl <870667674@qq.com>
2024-07-19 16:37:49 +0800

    dev

--- packages/dev/vite.config.ts
+++ packages/dev/vite.config.ts
@@ -2,9 +2,6 @@ import {defineConfig} from "vite"
 import vue            from "@vitejs/plugin-vue"
 
 export default defineConfig({
-  server: {
-    open: "index.html"
-  },
   plugins: [
     vue(),
   ],
